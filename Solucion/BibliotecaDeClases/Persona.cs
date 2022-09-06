4242331d238346ed83b59cd64b292db88674afa7
tobias galvez <you@example.com>
2022-09-05 22:21:36 -0300

    biblioteca de clases

---
+++ Solucion/BibliotecaDeClases/Persona.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BibliotecaDeClases
+{
+    public class Persona
+    {
+
+
+    }
+}
---
+++ Solucion/BibliotecaDeClases/Producto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClases
+{
+    public class Producto
+    {
+        string nombre;
+        float precio;
+        private int codigoDeProducto;
+
+        public Producto(string nombre, float precio)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+        }
+
+        public Producto(string nombre, float precio, int codigoDeProducto)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.codigoDeProducto = codigoDeProducto;
+            codigoDeProducto++;
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+        }
+
+        public float Precio { get { return precio; } }
+
+        public int CodigoDeProducto { get { return codigoDeProducto; } }
+
+    }
+}
