930bdeeb0e45aea9d140fc8cf9dda82883d3efc3
tobias galvez <you@example.com>
2022-09-05 22:32:07 -0300

    carpeta de ejercicios .net y c# creada

---
+++ Solucion/Ejercicios - Introduccion a NET y C#/Ejercicio_05 - Tirame un centro/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Ejercicio_05___Tirame_un_centro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeroIngresadoString;
+            int numeroIngresadoParseado;
+            int numeroInicial = 1;
+            int sumaNumerosAnteriores = 0;
+            int sumaNumerosPosteriores = 1;
+
+            string listaNumerosAnteriores="";
+            string listaNumerosPosteriores="";
+
+            Console.WriteLine("Ingrese un numero (mayor a 1): ");
+            numeroIngresadoString = Console.ReadLine();
+            while ((int.TryParse(numeroIngresadoString, out numeroIngresadoParseado) != true || numeroIngresadoParseado < 1))
+            {
+                Console.WriteLine("ERROR. ¡Opcion Invalida! Reingrese :");
+                numeroIngresadoString = Console.ReadLine();
+            }
+            do
+            {
+                for(int i = 0; i < numeroInicial; i++)
+                {
+                    sumaNumerosAnteriores += i;
+                    listaNumerosAnteriores += i + " | ";
+                }
+                for(int i=numeroInicial+1; i< numeroInicial * 2; i++)
+                {
+                    if(sumaNumerosAnteriores == sumaNumerosPosteriores)
+                    {
+                        Console.WriteLine($"{numeroInicial} es un centro numérico");
+                        Console.WriteLine($"Lista de numeros anteriores al centro numerico: {listaNumerosAnteriores}");
+                        Console.WriteLine($"Lista de numeros posteriores al centro numerico: {listaNumerosPosteriores}");
+                        break;
+                    }
+                    else
+                    {
+                        sumaNumerosPosteriores += i;
+                        listaNumerosPosteriores += i + " | ";
+                        
+                    }
+                }
+
+                numeroInicial = numeroInicial + 1;
+                sumaNumerosAnteriores = 0;
+                sumaNumerosPosteriores = 0;
+                listaNumerosAnteriores = " ";
+                listaNumerosPosteriores = " ";
+            } while (numeroInicial < numeroIngresadoParseado + 1);
+
+        }
+    }
+}
---
+++ Solucion/Ejercicios - Introduccion a NET y C#/Ejercicio_07 - Recibo de sueldo/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Ejercicio_07___Recibo_de_sueldo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string horaIngresadaString;
+            int horaIngresadaParseada;
+
+            string nombreIngresado;
+
+            string antiguedadIngresadaString;
+            int antiguedadIngresadaParseada;
+
+            string cantidadHorasTrabajadasString;
+            int cantidadHorasTrabajadasParseada;
+
+            int montoACobrar = 0;
+
+            int montoTotalACobrarEnBruto = 0;
+            float montoTotalACobrarNeto = 0;
+
+            string seguir = "si";
+
+            do
+            {
+                Console.WriteLine("Ingrese Hora: ");
+                horaIngresadaString = Console.ReadLine();
+                while ((int.TryParse(horaIngresadaString, out horaIngresadaParseada) != true) || horaIngresadaParseada <= 0)
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar hora! :");
+                    horaIngresadaString = Console.ReadLine();
+
+                }
+
+                Console.WriteLine("Ingrese nombre de empleado: ");
+                nombreIngresado = Console.ReadLine();
+
+
+                Console.WriteLine("Ingrese antiguedad (en años): ");
+               antiguedadIngresadaString = Console.ReadLine();
+                while ((int.TryParse(antiguedadIngresadaString, out antiguedadIngresadaParseada) != true) || antiguedadIngresadaParseada <= 0)
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar antiguedad! :");
+                    antiguedadIngresadaString = Console.ReadLine();
+
+                }
+
+                Console.WriteLine("Ingrese cantidad de horas trabajadas: ");
+                cantidadHorasTrabajadasString = Console.ReadLine();
+                while ((int.TryParse(cantidadHorasTrabajadasString, out cantidadHorasTrabajadasParseada) != true) || cantidadHorasTrabajadasParseada <= 0)
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar cantidad de horas trabajadas! :");
+                    cantidadHorasTrabajadasString = Console.ReadLine();
+
+                }
+
+
+                montoACobrar = horaIngresadaParseada * cantidadHorasTrabajadasParseada;
+
+                montoTotalACobrarEnBruto = montoACobrar + (cantidadHorasTrabajadasParseada * 150);
+
+                montoTotalACobrarNeto = ((float)(montoTotalACobrarEnBruto - (montoTotalACobrarEnBruto * 0.13)));
+
+                Console.WriteLine($"Nombre: {nombreIngresado}");
+                Console.WriteLine($"Antigüedad: {antiguedadIngresadaParseada}");
+                Console.WriteLine($"Valor hora: {horaIngresadaParseada}");
+                Console.WriteLine($"Total a cobrar en bruto: {montoTotalACobrarEnBruto}");
+                Console.WriteLine($"Total a cobrar neto: {montoTotalACobrarNeto}");
+
+
+                Console.WriteLine("Desea seguir ingresando empleados? (si/salir):");
+                seguir = Console.ReadLine();
+                while (seguir != "si" && seguir != "salir")
+                {
+                    Console.WriteLine("ERROR. Ingrese si/salir: ");
+                    seguir = Console.ReadLine();
+
+                }
+
+
+
+
+            } while (seguir == "si");
+            
+        }
+    }
+}
